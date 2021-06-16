ad0418155396f5c54d140199841a1ff8c5806228
Sergii Bogush <sergbogush@gmail.com>
2021-06-16 21:43:34 +0300

    change some files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
-import 'normalize.css'
+import 'normalize.css';
 import './sass/main.scss';
 
-import apiService from './js/api/apiService'
+import ApiService from './js/api/apiService';
 
-const api = new apiService();
-console.log(api.makeRequest());
+const apiService = new ApiService();
+const page = 1;
+getTrendingMovies();
+
+function getTrendingMovies() {
+  const res = apiService.getTrendingMovies(page).then(data => {
+    console.log(data);
+  });
+  console.log(res);
+}
