2007f7327885fa172008a24b7e5d10c9d34f9db3
Sergii Bogush <sergbogush@gmail.com>
2021-06-20 11:43:51 +0300

    add event click

--- src/js/lightBoxMovie.js
+++ src/js/lightBoxMovie.js
@@ -4,6 +4,11 @@ import notify from './error';
 
 const apiService = new ApiService();
 
+const refs = getRefs();
+console.log(refs.moviesContainer);
+
+refs.moviesContainer.addEventListener('click', onGalleryContainerClick);
+
 const movieId = 602063;
 
 console.log(movieInfoById(movieId));
@@ -20,3 +25,29 @@ function movieInfoById() {
       })
   );
 }
+
+function onGalleryContainerClick(evt) {
+  evt.preventDefault();
+
+  console.log(evt.target.classList);
+  const isGalleryMovieEl = evt.target.classList.contains('movie-image'); //проверка источника клика
+  if (!isGalleryMovieEl) {
+    return;
+  }
+  const openedMovie = evt.target;
+  console.log(openedMovie);
+  lightBoxOpen(openedMovie);
+}
+
+// function lightBoxOpen(image) {
+//   lightBoxContainerRef.classList.add('is-open');
+//   lightBoxImageRef.src = image.dataset.source;
+//   lightBoxImageRef.dataset.index = galleryImagesList.indexOf(image);
+//   window.addEventListener('keydown', onKeyPress);
+// }
+
+// function onBtnCloseLightBox() {
+//   lightBoxContainerRef.classList.remove('is-open');
+//   lightBoxImageRef.src = '';
+//   window.removeEventListener('keydown', onKeyPress);
+// }
