260729ea047f140795ea4d3731fb95b25e284524
Oleg Yamgurov <75248620+Jaguar838@users.noreply.github.com>
2021-06-21 18:16:15 +0300

    Delete src directory

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import 'normalize.css';
-import './sass/main.scss';
-
-import '@pnotify/core/dist/BrightTheme.css';
-import '@pnotify/core/dist/PNotify.css';
-
-import './js/main';
-import '/js/searchByQuery.js';
-import '/js/lightBoxMovie.js';
